a5d8b53ebe65ecf78a082f22f71b908ac9731761
GabrielBPID <gabrielbrunopereira.id@gmail.com>
2024-10-29 19:16:08 +0700

    adding tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,9 @@
         @endif
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <h1 class="text-8xl font-bold underline italic">
+            hello world!
+        </h1>
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
@@ -56,6 +59,7 @@
                                 @endauth
                             </nav>
                         @endif
+                        @vite('resources/css/app.css')
                     </header>
 
                     <main class="mt-6">
